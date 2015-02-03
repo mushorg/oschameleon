e29c9d62bddbcf7f3c87f666e26647a7e476b4e2
glaslos <lukas.rist@bluecoat.com>
2015-02-03 17:17:14 +0100

    fork root example

---
+++ test/root_fork.py
@@ -0,0 +1,38 @@
+import gevent.monkey
+gevent.monkey.patch_all()
+
+import os
+import pwd
+import grp
+
+import gevent
+
+
+def root_process():
+    while True:
+        print("Child running as {0}/{1}.".format(pwd.getpwuid(os.getuid())[0], grp.getgrgid(os.getgid())[0]))
+        gevent.sleep(1)
+
+
+def drop_privileges(uid_name='nobody', gid_name='nogroup'):
+    print("Running as {0}/{1}.".format(pwd.getpwuid(os.getuid())[0], grp.getgrgid(os.getgid())[0]))
+    wanted_uid = pwd.getpwnam(uid_name)[2]
+    wanted_gid = grp.getgrnam(gid_name)[2]
+
+    pid = gevent.fork()
+    if pid == 0:
+        os.setgid(wanted_gid)
+        os.setuid(wanted_uid)
+        new_uid_name = pwd.getpwuid(os.getuid())[0]
+        new_gid_name = grp.getgrgid(os.getgid())[0]
+        print("Privileges dropped, running as {0}/{1}.".format(new_uid_name, new_gid_name))
+    else:
+        lets = gevent.spawn(root_process)
+        gevent.joinall([lets, ])
+
+
+if __name__ == '__main__':
+    try:
+        drop_privileges()
+    except KeyboardInterrupt:
+        print "bye"

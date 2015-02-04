1ab446bd556d96fbb6c955b06f16249b613800f5
glaslos <lukas.rist@bluecoat.com>
2015-02-04 16:52:08 +0100

    Minor release 0.1.0 fixes

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 import oschameleon
 
 setup(
+    packages=["oschameleon", ],
     name=oschameleon.__title__,
     version=oschameleon.__version__,
     author='Glastopf Project',
     author_email='glaslos@gmail.com',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Topic :: Security",
+    ],
+    package_data={
+        "": ["*.txt", "*.md"],
+    },
+    include_package_data=True,
+    long_description=open('README.md').read(),
     url='https://github.com/glastopf/oschameleon',
     description='OS Fingerprint Obfuscation for modern Linux Kernels',
-    packages=find_packages(exclude=["*.pyc", ]),
+    test_suite='nose.collector',
+    tests_require="nose",
     zip_safe=False,
     install_requires=open('requirements.txt').read().splitlines(),
 )

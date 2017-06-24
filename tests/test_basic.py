f56ad479f7a0994a9d5acd82b8a87ffd3e448e1b
Lukas Rist <glaslos@gmail.com>
2017-06-24 09:52:58 +0200

    fixed test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -7,6 +7,6 @@ import unittest
 import oschameleon
 
 
-class TestFabricHealthRules(unittest.TestCase):
-    def test_module(self):
-self.assertTrue(oschameleon.__title__ == 'oschameleon')
+class TestBasic(unittest.TestCase):
+    def test_title(self):
+        self.assertTrue(oschameleon.__title__ == 'oschameleon')
